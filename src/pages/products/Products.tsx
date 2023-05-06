ea0a9aa57e6782312e4b5b72d529e8eb16c4a867
Juan Viveros <jviverosd1@gmail.com>
2023-05-06 17:21:23 -0500

    Products screen

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useLocation } from "react-router-dom";
+
+const products = [
+  {
+    name: "Coca Cola",
+    image:
+      "https://www.coca-cola.com.mx/content/dam/GO/CokeZone/Products/COCA-COLA-600ML.png",
+    description: "Refresco de cola",
+    price: 15,
+    category: "Bebidas",
+  },
+  {
+    name: "Pepsi",
+    image:
+      "https://www.pepsi.com/es-xl/uploads/sites/2/2020/09/pepsi-regular-2l.png",
+    description: "Refresco de cola",
+    price: 15,
+    category: "Bebidas",
+  },
+];
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const { find } = useLocation().search as any;
 
-export default Products
+  return (
+    <>
+      <div className="pt-40 pb-20 shadow gap-6 flex flex-col">
+        <p className="md:text-5xl font-black text-center">
+          Resultados para tu busqueda:
+        </p>
+        <p className="md:text-5xl font-black text-center">"{find}"</p>
+      </div>
+      <div className="bg-zinc-200">
+        {products.map((product) => (
+          <div className="flex gap-4 p-4">
+            <img src={product.image} alt={product.name} className="w-40" />
+            <div className="flex flex-col">
+              <p className="md:text-2xl font-semibold">{product.name}</p>
+              <p className="md:text-xl">{product.description}</p>
+              <p className="md:text-xl">${product.price}</p>
+
+              <button className="bg-zinc-100 p-2 rounded-md">
+                Agregar al carrito
+              </button>
+
+              <button className="bg-zinc-100 p-2 rounded-md">
+                Comprar ahora
+              </button>
+
+              <button className="bg-zinc-100 p-2 rounded-md">
+                Ver detalles
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Products;
