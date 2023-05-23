92d559199876dde406e707a822bff896b1b13ec1
Juan Viveros <jviverosd1@gmail.com>
2023-05-23 11:17:15 -0500

    a

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -45,6 +45,58 @@ const Products = () => {
             </p>
             <p className="md:text-5xl font-black text-center">"{find}"</p>
           </div>
+          {products?.[0] && (
+            <div className="my-20 mx-20 p-10 rounded-3xl bg-yellow-300">
+              <p className=" md:text-3xl font-bold mb-10">Producto destacado de la temporada:</p>
+              <div className="flex p-4 justify-between bg-zinc-200 rounded-xl mb-5 items-center">
+                <div className="flex gap-4">
+                  <div className="flex flex-col gap-4">
+                    <div>
+                      <p className="md:text-2xl font-semibold">
+                        {products?.[0]?.name}
+                      </p>
+                      <div className="md:text-xl font-medium flex gap-4">
+                        <div className="flex items-center">
+                          <GlobeAmericasIcon className="h-5 w-5 mr-1" />
+                          <p className="">
+                            {products?.[0]?.esgScore?.environment_score}
+                          </p>
+                        </div>
+                        <div className="flex items-center">
+                          <UserGroupIcon className="h-5 w-5 mr-1" />
+                          <p className="">
+                            {products?.[0]?.esgScore?.social_score}
+                          </p>
+                        </div>
+                        <div className="flex items-center">
+                          <ScaleIcon className="h-5 w-5 mr-1" />
+                          <p className="">
+                            {products?.[0]?.esgScore?.governance_score}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <p className="md:text-xl">{products?.[0]?.price}</p>
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <button
+                    className="bg-zinc-100 p-2 rounded-md"
+                    onClick={() => window.open(product.url, "_blank")}
+                  >
+                    Comprar
+                  </button>
+                  <button
+                    className="bg-zinc-100 p-2 rounded-md"
+                    onClick={() => window.open(product.url, "_blank")}
+                  >
+                    Ver detalles
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
           {products.map((product: any) => (
             <div className="flex p-4 justify-between bg-zinc-200 mx-20 rounded-xl mb-5 items-center">
               <div className="flex gap-4">
